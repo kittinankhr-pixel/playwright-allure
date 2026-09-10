b211e3b811836800e9859605deac622115f6a310
Kittinan <kittinan.khr@gmail.com>
2026-09-10 21:32:48 +0700

    Initial commit

---
+++ tests/Login.spec.js
@@ -0,0 +1,11 @@
+import { test, expect } from '@playwright/test';
+
+test('test', async ({ page }) => {
+  await page.goto('https://uat.me2plus.co.th/admin/');
+  await page.getByRole('textbox', { name: 'อีเมล' }).click();
+  await page.getByRole('textbox', { name: 'อีเมล' }).fill('Kittinan.Kh@me2plus.com');
+  await page.getByRole('textbox', { name: 'รหัสผ่าน' }).click();
+  await page.getByRole('textbox', { name: 'รหัสผ่าน' }).fill('Kittinan.Kh@me2plus.com');
+  await page.getByRole('button', { name: 'ลงชื่อเข้าใช้' }).click();
+  await expect(page).toHaveURL('https://uat.me2plus.co.th/admin/summary/');
+});
