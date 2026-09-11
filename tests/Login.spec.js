7d13e89c7cc535ef328797cd5a2fe2aa2fb990f6
Kittinan <kittinan.khr@gmail.com>
2026-09-11 11:03:23 +0700

    f

--- tests/Login.spec.js
+++ tests/Login.spec.js
@@ -5,7 +5,7 @@ test('Test Login111', async ({ page }) => {
   await page.getByRole('textbox', { name: 'อีเมล' }).click();
   await page.getByRole('textbox', { name: 'อีเมล' }).fill('Kittinan.Kh@me2plus.com');
   await page.getByRole('textbox', { name: 'รหัสผ่าน' }).click();
-  await page.getByRole('textbox', { name: 'รหัสผ่าน' }).fill('Kittinan.Kh@me2plus.com');
+  await page.getByRole('textbox', { name: 'รหัสผ่าน' }).fill('m');
   await page.getByRole('button', { name: 'ลงชื่อเข้าใช้' }).click();
   await expect(page).toHaveURL('https://uat.me2plus.co.th/admin/summary/');
 });
